c18de7b9780637ee4e44ae5892e965d0e6c66356
Константин Силенков <silenkov@inbox.ru>
2023-09-25 20:37:07 +0500

    Добавьте файлы проекта.

---
+++ HomeApi.Contracts/Models/Devices/RemoveDiveceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Models.Devices
+{
+    /// <summary>
+    /// Удаляет поддержку устройства.
+    /// </summary>
+    public class RemoveDiveceRequest
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+    }
+}
---
+++ HomeApi.Contracts/Models/Rooms/EditRoomRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Models.Rooms
+{
+    /// <summary>
+    /// Запрос для обновления свойств комнаты
+    /// </summary>
+    public class EditRoomRequest
+    {
+        public string NewName { get; set; }
+        public int NewArea { get; set; }
+        public bool NewGasConnected { get; set; }
+        public int NewVoltage { get; set; }
+    }
+}
